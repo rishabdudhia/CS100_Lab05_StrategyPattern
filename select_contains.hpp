e124930ef8fa9071d1e054c75eb49151e77541ec
fadi001 <fzaki001@ucr.edu>
2021-05-03 14:09:56 -0700

    Fixed code to a bool

--- select_contains.hpp
+++ select_contains.hpp
@@ -4,28 +4,34 @@
 
 using namespace std;
 
-class Select_Contains{
+class Select_Contains : public Select{
 	
 	public:
-		vector<T> Select_Contains(const Spreadsheet* sheet, string& column, string& name){
+		 Select_Contains(const Spreadsheet* sheet, string& column, string& name){
 			column_num = sheet->get_column_by_name(column);
-			if (column_num != -1){
-				for(unisgned int i = 0; i < data.size(); i++){
-					dataa = cell_data(i,column_num)
+			this->name = name;
+			}
+
+		  bool select(const Spreadsheet* sheet, int row) const{
+				if (column_num != -1){
+					dataa = cell_data(row,column_num)
 					//if(dataa.find(name) != std::string::npos){
 					if (dataa.contains(name){
-						rows_selected.pushback(i);
+						return true;
 					}
-					else{}
+					else{
+						return false;
 				}
+				else {return false;}
 			}
-		return rows_slected;
+		}
 
 
 	protected:
-		vector<int> rows_selected;	
+
     		int column_num;
-		string dataa;	
+		string dataa;
+		string name;	
 	
 
 }; 
