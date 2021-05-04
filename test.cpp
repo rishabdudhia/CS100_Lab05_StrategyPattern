3c27745b74af5c8a8db5b9222ae2d8c30bd19344
rishabdudhia <rishabdudhia@gmail.com>
2021-05-04 11:37:09 -0700

    Working still

--- test.cpp
+++ test.cpp
@@ -7,18 +7,6 @@
 
 using namespace std;
 
-TEST(SampleTests, testing_Not) {
-    Spreadsheet sheet;
-    sheet.set_column_names({"First", "Second", "Third"});
-    sheet.add_row({"one", "two", "three"});
-    sheet.add_row({"four", "five", "six"});
-    sheet.add_row({"seven", "eight", "nine"});
-
-    string expected = "one two three \nseven eight nine \n";
-    stringstream out;
-    sheet.set_selection(new Select_Contains(&sheet, "First", "e"));
-    EXPECT_EQ(out.str(), expected);
-}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
