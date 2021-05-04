b79739b964d770a436b6033da8077070669afad1
rishabdudhia <rishabdudhia@gmail.com>
2021-05-04 11:40:39 -0700

    Put in select header

--- select.hpp
+++ select.hpp
@@ -38,4 +38,31 @@ public:
     virtual bool select(const std::string& s) const = 0;
 };
 
+class Select_Or : public Select {
+  private:
+        Select* left;
+        Select* right;
+  public:
+        Select_Or(Select* inLeft, Select* inRight) {
+            left = inLeft;
+            right = inRight;
+        }
+
+        ~Select_Or() {
+            delete left;
+            delete right;
+        }
+
+        bool select(const Spreadsheet* sheet, int row) const {
+            bool leftSel = left->select(sheet, row);
+            bool rightSel = right->select(sheet,row);
+
+            if (leftSel || rightSel)
+                return true;
+            else
+                return false;
+        }
+};
+
+
 #endif //__SELECT_HPP__
